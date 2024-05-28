8a5c5e6bdb934126298528df15a34dcb18096f79
Ernaw <contact@erwanmarie.fr>
2024-05-28 10:09:25 +0200

    FINAL

--- main.py
+++ main.py
@@ -12,12 +12,12 @@ def add (first, second):
     return first + second
 
 #blabladadaz
-
-option = input("On fait quoi  ? (/,*,+)  \n")
-print(f"My option {option}" )
-option = {
-    "+" : print(add(int(input("Nbr: ")), int(input("Nbr2: ")))),
-    "*" : print(multiple(int(input("Nbr: ")), int(input("Nbr2: ")))),
-    "/" : print(div(int(input("Nbr: ")), int(input("Nbr2: ")))),
-}
+def gjgirejngeri():
+    option = input("On fait quoi  ? (/,*,+)  \n")
+    print(f"My option {option}" )
+    option = {
+        "+" : print(add(int(input("Nbr: ")), int(input("Nbr2: ")))),
+        "*" : print(multiple(int(input("Nbr: ")), int(input("Nbr2: ")))),
+        "/" : print(div(int(input("Nbr: ")), int(input("Nbr2: ")))),
+    }
     
