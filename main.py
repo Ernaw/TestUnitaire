ca41d9afa0091524ec0b54b5bd9c47161da6e653
Ernaw <contact@erwanmarie.fr>
2024-05-28 11:57:51 +0200

    TEST1

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import pytest
 def div(first, second):
     assert (second != 0)
     return first / second
-def multiple(first, second): #D
+def multiple(first, second): #DD
     assert(first != 0)
     assert(second != 0)
     return first * second
